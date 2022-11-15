b26b45dc13daf9b5d861697cb0443cdfcdf073be
Loner1024 <xth12138@gmail.com>
2022-11-16 00:48:44 +0800

    feat:conversions

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -6,6 +6,9 @@
 // Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for a hint.
 
 use std::convert::{TryFrom, TryInto};
+use std::num::ParseIntError;
+use std::ops::Index;
+use std::thread::sleep;
 
 #[derive(Debug, PartialEq)]
 struct Color {
@@ -23,8 +26,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -38,6 +39,12 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red,green,blue) = (
+            u8::try_from(tuple.0).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(tuple.1).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(tuple.2).or(Err(IntoColorError::IntConversion))?,
+        );
+        Ok(Color{red,green,blue})
     }
 }
 
@@ -45,6 +52,12 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let (red,green,blue) = (
+            u8::try_from(arr[0]).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(arr[1]).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(arr[2]).or(Err(IntoColorError::IntConversion))?,
+        );
+        Ok(Color{red,green,blue})
     }
 }
 
@@ -52,6 +65,10 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        match slice {
+            [r,g,b] => Color::try_from((*r,*g,*b)),
+            _=>Err(IntoColorError::BadLen)
+        }
     }
 }
 
